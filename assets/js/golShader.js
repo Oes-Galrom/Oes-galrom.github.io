47ccbeae0ae7fc6799c9c6dc68193bc0c197c18c
unknown <oscargalrom@gmail.com>
2024-02-06 10:45:15 -0500

    shader fix - properly sending textures

---
+++ assets/js/golShader.js
@@ -0,0 +1,29 @@
+let golShader;
+
+function preload() {
+    golshader = loadShader('gol.vert, 'gol.frag');
+}
+
+function setup() {
+    createCanvas(windowWidth, windowHeight, WEBGL);//include WEBGL
+    pixelDensity(1);// 1 pixel
+    noSmooth;// no antiliasing
+    background(0); //black background
+    stroke(255); //white 
+    shader(golShader);
+    golShader.setUniform("normalRes", [1.0/width, 1.0/height]);//normal res is a vector with size of the pixel normalized between 0 and 1 to calculate neighbords in the shader
+}
+
+function draw() {
+    if(mouseIsPressed) {
+        line(
+            pmouseX-width/2,
+            pmouseY-height/2,
+            mouseX-width/2,
+            mouseY-height/2
+        )
+    }
+    golShader.setUniform('tex', get());//pass the state as a texture
+    Reflect(-width/2,-height/2,width,height);
+
+}
--- assets/shaders/vertex.vert
+++
@@ -1,6 +0,0 @@
-// vertex shader
-attribute vec3 aPosition;
-void main() {
-  vec4 positionVec4 = vec4(aPosition, 1.0);
-  gl_Position = positionVec4;
-}
