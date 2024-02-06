fd4e8610ea4a6bac4fe3d599077b00017c57c930
unknown <oscargalrom@gmail.com>
2024-02-06 12:24:21 -0500

    fixing draw

--- assets/js/golShader.js
+++ assets/js/golShader.js
@@ -22,20 +22,22 @@ function setup() {
 }
 
 function draw() {
-  if(mouseIsPressed) {
-    line(
-      pmouseX-width/2,
-      pmouseY-height/2,
-      mouseX-width/2,
-      mouseY-height/2
-    );
-  }  
+    if(mouseIsPressed) {
+      line(
+        pmouseX-width/2,
+        pmouseY-height/2,
+        mouseX-width/2,
+        mouseY-height/2
+      );
+    }
   
-  // Copy the rendered image into our prevFrame image
-  prevFrame.image(get(), 0, 0);  
-  // Set the image of the previous frame into our shader
-  golShader.setUniform('tex', prevFrame);
-  prevFrame.shader(golShader);
-  // Give the shader a surface to draw on
-  rect(-width/2,-height/2,width,height);
-}
+    // Set the shader and uniform for prevFrame
+    prevFrame.shader(golShader);
+    golShader.setUniform('tex', prevFrame);
+    
+    // Render on prevFrame
+    prevFrame.rect(-width/2, -height/2, width, height);
+  
+    // Draw the prevFrame onto the main canvas
+    image(prevFrame, -width/2, -height/2, width, height);
+  }
