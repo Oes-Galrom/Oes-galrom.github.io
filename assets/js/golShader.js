26c4d9217cbc8ca316a1b15bf0aff4d63a0449a6
unknown <oscargalrom@gmail.com>
2024-02-06 10:54:16 -0500

    misc patchup

--- assets/js/golShader.js
+++ assets/js/golShader.js
@@ -1,29 +1,41 @@
 let golShader;
 
+let prevFrame;
+
 function preload() {
-    golShader = loadShader('gol.vert', 'gol.frag');
-  }
+  golShader = loadShader('gol.vert', 'gol.frag');
+}
 
 function setup() {
-    createCanvas(windowWidth, windowHeight, WEBGL);//include WEBGL
-    pixelDensity(1);// 1 pixel
-    noSmooth;// no antiliasing
-    background(0); //black background
-    stroke(255); //white 
-    shader(golShader);
-    golShader.setUniform("normalRes", [1.0/width, 1.0/height]);//normal res is a vector with size of the pixel normalized between 0 and 1 to calculate neighbords in the shader
+  createCanvas(600, 600, WEBGL);
+  pixelDensity(1);// 1 pixel
+  noSmooth();// no antiliasing
+  
+  prevFrame = createGraphics(width, height);
+  prevFrame.pixelDensity(1);
+  prevFrame.noSmooth();
+  
+  background(0);//black background
+  stroke(255); //white 
+  shader(golShader);
+  golShader.setUniform("normalRes", [1.0/width, 1.0/height]); //normal res is a vector with size of the pixel normalized between 0 and 1 to calculate neighbords in the shader
 }
 
 function draw() {
-    if(mouseIsPressed) {
-        line(
-            pmouseX-width/2,
-            pmouseY-height/2,
-            mouseX-width/2,
-            mouseY-height/2
-        )
-    }
-    golShader.setUniform('tex', get());//pass the state as a texture
-    Reflect(-width/2,-height/2,width,height);
-
+  if(mouseIsPressed) {
+    line(
+      pmouseX-width/2,
+      pmouseY-height/2,
+      mouseX-width/2,
+      mouseY-height/2
+    );
+  }  
+  
+  // Copy the rendered image into our prevFrame image
+  prevFrame.image(get(), 0, 0);  
+  // Set the image of the previous frame into our shader
+  golShader.setUniform('tex', prevFrame);
+  
+  // Give the shader a surface to draw on
+  rect(-width/2,-height/2,width,height);
 }
