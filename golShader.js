367f8f6fc26e2b39ceb9c97504c87d8e4c2cadc3
Oes-Galrom <141342753+Oes-Galrom@users.noreply.github.com>
2025-01-18 19:26:48 -0500

    attempting to make it so there a central scroll column

--- golShader.js
+++ golShader.js
@@ -1,5 +1,4 @@
 let golShader;
-
 let prevFrame;
 
 function preload() {
@@ -7,39 +6,41 @@ function preload() {
 }
 
 function setup() {
-  createCanvas(windowWidth, windowHeight, WEBGL);
+  // create the canvas so it fits the container
+  const canvasParent = document.getElementById('canvas-container');
+  let c = createCanvas(windowWidth, windowHeight, WEBGL);
+  c.parent(canvasParent);
+
   pixelDensity(1);
   noSmooth();
-  
+
   prevFrame = createGraphics(width, height);
   prevFrame.pixelDensity(1);
   prevFrame.noSmooth();
-  
-  background(0);
-  stroke(255);
+
   shader(golShader);
   golShader.setUniform("normalRes", [1.0/width, 1.0/height]);
+  background(0);
+  stroke(255);
 }
 
 function windowResized(){
   resizeCanvas(windowWidth, windowHeight);
+  prevFrame.resizeCanvas(width, height);
+  golShader.setUniform("normalRes", [1.0/width, 1.0/height]);
 }
 
 function draw() {
-  if(mouseIsPressed) {
+  if (mouseIsPressed) {
     line(
-      pmouseX-width/2,
-      pmouseY-height/2,
-      mouseX-width/2,
-      mouseY-height/2
+      pmouseX - width / 2,
+      pmouseY - height / 2,
+      mouseX - width / 2,
+      mouseY - height / 2
     );
-  }  
-  
-  // Copy the rendered image into our prevFrame image
-  prevFrame.image(get(), 0, 0);  
-  // Set the image of the previous frame into our shader
+  }
+
+  prevFrame.image(get(), 0, 0);
   golShader.setUniform('tex', prevFrame);
-  
-  // Give the shader a surface to draw on
-  rect(-width/2,-height/2,width,height);
+  rect(-width/2, -height/2, width, height);
 }
